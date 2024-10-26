2caa36c8c02c3c86568fbe9f467880a7aa6f31ba
fajar siagian <fajar7xx@gmail.com>
2024-10-26 19:29:09 +0700

    first commit 2

---
+++ eslint.config.js
@@ -0,0 +1,56 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-config-prettier'
+
+export default [
+  { ignores: [
+      // React template ignores
+      '.DS_*',
+      '*.log',
+      'logs',
+      '**/*.backup.*',
+      '**/*.back.*',
+      'node_modules',
+      'bower_components',
+      '*.sublime*',
+      'psd',
+      'thumb',
+      'sketch',
+      'vite.config.js',
+      'dist',
+      '.env*'
+    ] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: 'detect' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      ...prettier.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+]
